19fc24f7d05c61d7f24d9c0126f8eb7a7c1edc8d
arkady <avjf@att.net>
2021-04-04 17:54:05 -0500

    Pagination

--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -7,25 +7,27 @@
       :key="item.node.id"
       class="blog-post"
     >
-
-    <div class="media my-5">
-      <!-- <g-image immediate :src="item.node.image" class="mr-3" alt="image" /> -->
-      <div class="media-body">
-        <g-link :to="item.node.path">
-          <h4 class="mt-0">{{item.node.title}}</h4>
-        </g-link>
-        <p class="text-dark mb-2" style="font-weight: 500">By {{item.node.author}} - {{item.node.date | luxon}}</p>
-        <p class="text-dark">{{item.node.excerpt}}</p>
+      <div class="media my-5">
+        <!-- <g-image immediate :src="item.node.image" class="mr-3" alt="image" /> -->
+        <div class="media-body">
+          <g-link :to="item.node.path">
+            <h4 class="mt-0">{{item.node.title}}</h4>
+          </g-link>
+          <p class="text-dark mb-2" style="font-weight: 500">By {{item.node.author}} - {{item.node.date | luxon}}</p>
+          <p class="text-dark">{{item.node.excerpt}}</p>
+        </div>
       </div>
     </div>
 
-    </div>
+    <Pager :info="$page.posts.pageInfo"
+       class="pager-container"
+       linkClass="pager-container__link"  />
   </Layout>
 </template>
 
 <page-query>
-query Articles {
-	posts: allArticle(sortBy: "date") {
+query ($page: Int) {
+	posts: allArticle(sortBy: "date", perPage: 3, page: $page) @paginate {
     edges {
       node {
         id
@@ -37,14 +39,29 @@ query Articles {
         date
       }
     }
+    pageInfo {
+      perPage
+      currentPage
+      totalPages
+      totalItems
+      hasPreviousPage
+      hasNextPage
+      isFirst
+      isLast
+    }
   }
 }
 </page-query>
 
 <script>
+import { Pager } from 'gridsome';
+
 export default {
   metaInfo: {
     title: 'Articles'
+  },
+  components: {
+    Pager
   }
 }
 </script>
@@ -59,5 +76,33 @@ export default {
 
 .media-body {
   margin: auto;
+}
+
+.pager-container {
+  display: inline-block;
+  font-size: 1.2rem;
+  text-align: center;
+  width: 100%;
+  color: black;
+  opacity: .9;
+
+  &__link {
+    text-align: center;
+    padding: 0.6rem 1.2rem;
+    color: white;
+    text-decoration: none;
+  }
+}
+
+.pager-container .active {
+  background-color:  #224C98;
+}
+
+.pager-container :not(.active) {
+  background-color: rgb(134, 136, 158);  
+}
+
+.pager-container__link {
+  margin-right: 4px;
 }
 </style>
