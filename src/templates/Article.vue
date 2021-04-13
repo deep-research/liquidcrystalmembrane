a93d73806fa4f3c6c4a12f6c0a28d28ca692bdb0
arkady <avjf@att.net>
2021-04-12 19:01:13 -0500

    Tag Links on Article Page

--- src/templates/Article.vue
+++ src/templates/Article.vue
@@ -11,8 +11,20 @@
 
       <h2 v-html="$page.post.title" />
       <p class="mb-0" style="font-size: 20px">By {{$page.post.author}} - {{$page.post.date | luxon}}</p>
-      <p v-if="$page.post.category" class="mb-0 mt-1"><span style="font-weight: 500">Category:</span> {{$page.post.category.title}}</p>
-      <p v-if="this.getTags()" class="mb-0 mt-1"><span style="font-weight: 500">Tags:</span> {{this.getTags()}}</p>
+      <p v-if="$page.post.category" class="mb-0 mt-1">
+        <span style="font-weight: 500">
+          <g-link to="/categories">Category</g-link>:
+        </span> <g-link :to="$page.post.category.path">{{$page.post.category.title}}</g-link>
+      </p>
+      <p v-if="$page.post.tags.length > 0" class="mb-0 mt-1">
+        <span style="font-weight: 500">
+          <g-link to="/tags">Tags</g-link>:
+        </span>
+          <span v-for="(tag, index) in $page.post.tags" :key="index">
+            <g-link :to="tag.path">{{tag.title}}</g-link>
+          <span v-if="index+1 < $page.post.tags.length">, </span>
+        </span>
+      </p>
       <!-- <div><b>Time:</b> {{ $page.post.timeToRead }} min read</div> -->
       <!-- <BlogContent class="mt-5" :content="$page.post.content"/> -->
       <VueRemarkContent class="mt-5" />
@@ -34,9 +46,11 @@ query Article ($path: String!) {
     path
     category {
       title
+      path
     }
     tags {
       title
+      path
     }
   }
 }
@@ -53,17 +67,17 @@ export default {
     return {
       title: this.$page.post.title,
     }
-  },
-  methods: {
-    getTags () {
-      let tags = []
-      this.$page.post.tags.forEach(tag => {
-        tags.push(tag.title)
-      })
-        
-      return(tags.join(', '))
-    }
   }
+  // methods: {
+  //   getTags () {
+  //     let tags = []
+  //     this.$page.post.tags.forEach(tag => {
+  //       tags.push(tag.title)
+  //     })
+        
+  //     return(tags)
+  //   }
+  // }
 }
 </script>
 
