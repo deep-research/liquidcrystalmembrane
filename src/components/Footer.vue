075c914d869c524dfe4172a0ad06dfe50932ef5e
Victor <fisheve@mx.lakeforest.edu>
2021-04-22 23:58:32 -0500

    Back To Top HIghlightable

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,16 @@
 <template>
-  <footer class="page-footer font-small teal py-4">
-    <div style="max-width: 950px; width: 90vw; margin: 0 auto;" class="d-flex justify-content-between align-items-center"> 
-      <div class="footer-copyright">© {{ new Date().getFullYear() }} Arkady Fisher</div>
-      <b-button variant="link" size="sm" @click="top()" style="font-size: 16px">Back To Top</b-button>
+  <footer
+    class="page-footer font-small teal py-4" >
+    <div
+      style="max-width: 950px; width: 90vw; margin: 0 auto;"
+      class="d-flex justify-content-between align-items-center" > 
+    <div class="footer-copyright">
+      © {{ new Date().getFullYear() }} Arkady Fisher
+    </div>
+    <b-button
+      variant="link" size="sm" @click="top()" id="back-to-top" >
+      Back To Top
+    </b-button>
     </div>
   </footer>
 </template>
@@ -22,5 +30,13 @@ footer {
   width: 100%;
   margin-top: 6rem;
   background-color: #f5f5f5;
+}
+
+#back-to-top {
+  font-size: 16px;
+  -webkit-user-select: auto; /* Safari */        
+  -moz-user-select: auto; /* Firefox */
+  -ms-user-select: auto; /* IE10+/Edge */
+  user-select: auto; /* Standard */
 }
 </style>
