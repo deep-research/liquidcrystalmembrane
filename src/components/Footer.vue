7cad5e8d6dd19e7584fae9bfad6a0ad16703657f
Victor <fisheve@mx.lakeforest.edu>
2021-04-22 19:05:27 -0500

    Custom Back To Top Button in Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,21 @@
 <template>
   <footer class="page-footer font-small teal py-4">
-    <div class="footer-copyright text-center">© {{ new Date().getFullYear() }} Arkady Fisher
+    <div style="max-width: 982px; width: 90vw; margin: 0 auto;" class="d-flex justify-content-between align-items-center"> 
+      <div class="footer-copyright">© {{ new Date().getFullYear() }} Arkady Fisher</div>
+      <b-button @click="top()">Back To Top</b-button>
     </div>
   </footer>
 </template>
+
+<script>
+export default {
+  methods: {
+    top() {
+      window.scrollTo(0,0)
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 footer {
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -12,29 +12,10 @@
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
-import { addBackToTop } from 'vanilla-back-to-top'
-
 export default {
   components: {
     Footer,
     Header
-  },
-  mounted() {
-    addBackToTop({
-      backgroundColor: '#224c98',
-      cornerOffset: 20, // px
-      diameter: 40, // px
-      // ease: inOutSine, // any one from https://www.npmjs.com/package/ease-component will do
-      id: 'back-to-top',
-      innerHTML: '<svg viewBox="0 0 24 24"><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path></svg>',
-      onClickScrollTo: 0, // px
-      scrollContainer: document.body, // or a DOM element, e.g., document.getElementById('content')
-      scrollDuration: 100, // ms
-      showWhenScrollTopIs:  2500, // px
-      // size: diameter, // alias for diameter
-      textColor: '#fff',
-      zIndex: 1
-    })
   }
 }
 </script>
