4eb7c82ad343d7b029bab057aeb14e87fe06e351
afisher4 <avjf14@gmail.com>
2024-07-28 19:15:17 -0500

    Update About.vue

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -9,7 +9,8 @@
       <li><a target="_blank" href="https://t.me/newspsychicforum">News Psychic Telegram Forum</a></li>
     </ul>
 
-    <br /><br />
+    <br />
+    
     <img style="max-width: 200px" class="about-image pb-0 mb-5" src="../../uploads/atom.svg" />
     <div style="display: none">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
    
